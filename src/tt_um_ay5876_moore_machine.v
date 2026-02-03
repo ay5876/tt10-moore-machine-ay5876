dbd19f0189fdda93359e4b5c7b4bd095aa31ccfb
ay5876 <ay5876@wayne.edu>
2026-02-03 11:14:54 -0500

    Update tt_um_ay5876_moore_machine.v

--- src/tt_um_ay5876_moore_machine.v
+++ src/tt_um_ay5876_moore_machine.v
@@ -19,56 +19,53 @@ module tt_um_ay5876_moore_machine (
 
     // State parameters 
     parameter state_a=3'b000, state_b=3'b010, state_c=3'b110, state_d=3'b100, state_e=3'b011;
- always@(posedge clk)
- begin
- if(~rst_n)
-y<=state_a;
- else
- y<=next_state;
- end
- assign z1 = ~clk & y[3];
- always@(y or x1)
- begin
- case(y)
- state_a:
- if(x1==0)
- next_state=state_a;
- else
- next_state=state_b;
- state_b:
- if(x1==0)
- next_state=state_a;
- else
- next_state=state_c;
- state_c:
- if(x1==0)
- next_state=state_d;
- else
- next_state=state_c;
- state_d:
- if(x1==0)
- next_state=state_a;
- else
- next_state=state_e;
- state_e:
- if(x1==0)
-next_state=state_a;
- else
- next_state=state_c;
- default:next_state=state_a;
- endcase
- end
- // All output pins must be assigned. If not used, assign to 0.
- assign uo_out[0] = y[1];
- assign uo_out[1] = y[2];
- assign uo_out[2] = y[3];
- assign uo_out[3] = z1;
- assign uo_out[4] = 1'b0;
- assign uo_out[5] = 1'b0;
- assign uo_out[6] = 1'b0;
- assign uo_out[7] = 1'b0;
- assign uio_out = 0;
- assign uio_oe = 0;
- // List all unused inputs to prevent warnings
- wire _unused = &{ena, ui_in[7:1], uio_in, 1'b0};
+
+    // Sequential State Update
+    always @(posedge clk) begin
+        if (~rst_n)
+            y <= state_a;
+        else
+            y <= next_state;
+    end
+
+    // Moore Output Logic: Depends only on the state register 'y'
+    assign z1 = y[3];
+
+    // Combinational Next State Logic
+    always @(y or x1) begin
+        case(y)
+            state_a: begin
+                if(x1 == 0) next_state = state_a;
+                else        next_state = state_b;
+            end
+            state_b: begin
+                if(x1 == 0) next_state = state_a;
+                else        next_state = state_c;
+            end
+            state_c: begin
+                if(x1 == 0) next_state = state_d;
+                else        next_state = state_c;
+            end
+            state_d: begin
+                if(x1 == 0) next_state = state_a;
+                else        next_state = state_e;
+            end
+            state_e: begin
+                if(x1 == 0) next_state = state_a;
+                else        next_state = state_c;
+            end
+            default: next_state = state_a;
+        endcase
+    end
+
+    // Pin Assignments (Required for Tiny Tapeout Linter)
+    assign uo_out[0] = y[1];
+    assign uo_out[1] = y[2];
+    assign uo_out[2] = y[3];
+    assign uo_out[3] = z1;
+    assign uo_out[7:4] = 4'b0000;
+    
+    assign uio_out = 8'b00000000;
+    assign uio_oe  = 8'b00000000;
+
 endmodule
