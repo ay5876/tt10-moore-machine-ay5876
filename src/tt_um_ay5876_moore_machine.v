354d3eb55d3e739d883a41eaffcd4d001ff1dd66
ay5876 <ay5876@wayne.edu>
2026-02-03 11:02:59 -0500

    Update tt_um_ay5876_moore_machine.v

--- src/tt_um_ay5876_moore_machine.v
+++ src/tt_um_ay5876_moore_machine.v
@@ -1,15 +1,13 @@
 `default_nettype none
 module tt_um_ay5876_moore_machine (
- input wire [7:0] ui_in, // Dedicated inputs
- output wire [7:0] uo_out, // Dedicated outputs
- input wire [7:0] uio_in, // IOs: Input path
- output wire [7:0] uio_out, // IOs: Output path
- output wire [7:0] uio_oe, // IOs: Enable path (active high:
-0=input, 1=output)
- input wire ena, // always 1 when the design is powered,
-so you can ignore it
- input wire clk, // clock
- input wire rst_n // reset_n - low to reset
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path
+    input  wire       ena,      // always 1
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
 );
  // input
  wire x1 = ui_in[0];
